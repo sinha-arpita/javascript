83626d1cb1f194cbe38c1a575224d902ff855181
Alok Kumar Sinha (alokk) <alokk@cisco.com>
2018-07-27 17:34:43 -0700

    ...

--- script1.js
+++
@@ -1,8 +0,0 @@
-var qset = [{question: "what is the color of stop sign?"<br/>
-             "a.blue" <br/>  "b.green "
-             "c.red"<br/> 
-             "answer:"c" "};
-             {question: "what is the color of sun sign?"<br/>
-             "a.yellow" <br/>  "b.green "
-             "c.red"<br/> 
-             "answer:"a" "};]
